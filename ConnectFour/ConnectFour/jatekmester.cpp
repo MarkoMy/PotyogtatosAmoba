310bb256580906272083ec04fc2880d9e9bf81d6
MarkoMy <96284338+MarkoMy@users.noreply.github.com>
2023-05-21 15:08:14 +0200

    szépítés

--- ConnectFour/ConnectFour/jatekmester.cpp
+++ ConnectFour/ConnectFour/jatekmester.cpp
@@ -2,52 +2,50 @@
 
 #include <vector>
 
-std::vector<int> JatekMester::TalbaGet(){
+std::vector<int> JatekMester::TalbaGet() {
     return tabla;
 }
 
-
-void JatekMester::Move(int x){
-
-    if(_gameRun && tabla[x] == EMPTY){
-                int oszlop = x % 7;
-                for(int i=oszlop; i < oszlop+5*7; i+=7){
-                    if(tabla[i] == EMPTY && tabla[oszlop+5*7] == EMPTY){
-                        tabla[oszlop+5*7] = _player;
-                        _player = !_player;
-                        lastPlace = oszlop+5*7;
-                        break;
-                    }else if(tabla[i] == EMPTY && tabla[i+7] != EMPTY){
-                        tabla[i] = _player;
-                        _player = !_player;
-                        lastPlace = i;
-                    }
-                }
-
-    }
-
+int JatekMester::GetText() {
+    return AllText;
 }
 
-void JatekMester::CheckText(){
-    if(!_player){
+void JatekMester::Move(int x) {
+    if (_gameRun && tabla[x] == EMPTY) {
+        int oszlop = x % 7;
+        for (int i = oszlop; i < oszlop + 5 * 7; i += 7) {
+            if (tabla[i] == EMPTY && tabla[oszlop + 5 * 7] == EMPTY) {
+                tabla[oszlop + 5 * 7] = _player;
+                _player = !_player;
+                lastPlace = oszlop + 5 * 7;
+                break;
+            } else if (tabla[i] == EMPTY && tabla[i + 7] != EMPTY) {
+                tabla[i] = _player;
+                _player = !_player;
+                lastPlace = i;
+                break;
+            }
+        }
+    }
+}
+
+void JatekMester::CheckText() {
+    if (!_player) {
         AllText = Y;
-    } else if(_player){
+    } else {
         AllText = R;
     }
 
-    if(!_gameRun && _player){
+    if (!_gameRun && _player) {
         AllText = YW;
-    } else if(!_gameRun && !_player){
+    } else if (!_gameRun && !_player) {
         AllText = RW;
     }
 
-    if(CheckTie()){
-        AllText = D; _gameRun = !_gameRun;
+    if (CheckTie()) {
+        AllText = D;
+        _gameRun = false;
     }
-}
-
-int JatekMester::GetText(){
-    return AllText;
 }
 
 void JatekMester::checkWin() {
@@ -55,35 +53,39 @@ void JatekMester::checkWin() {
     int sor = index / 7;
     int oszlop = index % 7;
 
-    //sor
+    // Sor
     int szamlalo = 0;
-    for(int i=sor*7; i < sor*7+7; i++){
-        if(tabla[i] != EMPTY){
-        if(tabla[i] == tabla[i+1] ){
-            szamlalo++;
-        }
-        if(tabla[i] != tabla[i+1])
-            szamlalo = 0;
-        if(szamlalo == 3)
-        _gameRun = 0;
+    for (int i = sor * 7; i < sor * 7 + 7; i++) {
+        if (tabla[i] != EMPTY) {
+            if (tabla[i] == tabla[i + 1]) {
+                szamlalo++;
+            } else {
+                szamlalo = 0;
+            }
+            if (szamlalo == 3) {
+                _gameRun = false;
+                break;
+            }
         }
     }
 
-    //oszlop
+    // Oszlop
     szamlalo = 0;
-    for(int i=oszlop; i < oszlop+5*7; i+=7){
-        if(tabla[i] != EMPTY){
-        if(tabla[i] == tabla[i+7] ){
-            szamlalo++;
-        }
-        if(tabla[i] != tabla[i+7])
-            szamlalo = 0;
-        if(szamlalo == 3)
-        _gameRun = 0;
+    for (int i = oszlop; i < oszlop + 5 * 7; i += 7) {
+        if (tabla[i] != EMPTY) {
+            if (tabla[i] == tabla[i + 7]) {
+                szamlalo++;
+            } else {
+                szamlalo = 0;
+            }
+            if (szamlalo == 3) {
+                _gameRun = false;
+                break;
+            }
         }
     }
 
-    //átló le-jobb
+    // Átló le-jobb
     szamlalo = 0;
     int r = sor;
     int c = oszlop;
@@ -93,74 +95,57 @@ void JatekMester::checkWin() {
         c--;
     }
     while (r <= 5 && c <= 6) {
-            int currentIndex = r * 7 + c;
-            if (tabla[currentIndex] == tabla[index]&& tabla[index] != EMPTY) {
-                szamlalo++;
-                if (szamlalo >= 4)
-                   _gameRun = 0;
-            } else {
-                szamlalo = 0;
+        int currentIndex = r * 7 + c;
+        if (tabla[currentIndex] == tabla[index] && tabla[index] != EMPTY) {
+            szamlalo++;
+            if (szamlalo >= 4) {
+                _gameRun = false;
+                break;
             }
-            r++;
-            c++;
+        } else {
+            szamlalo = 0;
         }
+        r++;
+        c++;
+    }
 
-    //átló le-bal
-
+    // Átló le-bal
     szamlalo = 0;
     r = sor;
     c = oszlop;
 
-    while (r > 0 && c < 0) {
+    while (r > 0 && c < 6) {
         r--;
         c++;
     }
     while (r <= 5 && c >= 0) {
-            int currentIndex = r * 7 + c;
-            if (tabla[currentIndex] == tabla[index] && tabla[index] != EMPTY) {
-                szamlalo++;
-                if (szamlalo >= 4)
-                   _gameRun = 0;
-            } else {
-                szamlalo = 0;
+        int currentIndex = r * 7 + c;
+        if (tabla[currentIndex] == tabla[index] && tabla[index] != EMPTY) {
+            szamlalo++;
+            if (szamlalo >= 4) {
+                _gameRun = false;
+                break;
             }
-            r++;
-            c--;
+        } else {
+            szamlalo = 0;
         }
+        r++;
+        c--;
+    }
 }
-
-
 
 bool JatekMester::CheckTie() {
     for (int i = 0; i < 42; ++i) {
-        if (tabla[i] == EMPTY)
+        if (tabla[i] == EMPTY) {
             return false;
+        }
     }
     return true;
 }
 
-void JatekMester::gameReseter(){
-    for (int i = 0; i < 42; ++i) {
-        tabla[i] = EMPTY;
-    }
-    _player = 0;
-    _gameRun = 1;
+void JatekMester::gameReseter() {
+    tabla.assign(42, EMPTY);
+    _player = false;
+    _gameRun = true;
     CheckText();
 }
-
-
-/*
- * KÉNE:
- * winchecks
- *
- * ha az összes mező tele, akkor TIE
- *
- * checkelni azt, hogy 4 ugyanolyan van
- *
- * PIPA:
- * csak azokat a kattintásokat engedni, ami EMPTY, és alatta nem EMPTY
- * Csak akkor rak le, ha _gameRun true, ez kell majd a játék végéhez
- *
- * Reset gomb
- * Kiírni, hogy melyik ember jön
- */
